676a7e018a0112addb7a8f071f6b4f91400a4940
Samuel-Githugu <githugu.samuel@gmail.com>
2020-10-02 23:40:32 +0300

    friday night, 2/09

--- app.js
+++ app.js
@@ -35,11 +35,10 @@ function categoryDrop() {
   }
 
 
-  var modal = document.getElementById('id01');
+  var modal1 = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal1) {
         modal.style.display = "none";
     }
 }
